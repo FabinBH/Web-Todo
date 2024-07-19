1b3de60609e4da6f394ac05735aa6bdac826cef6
Fabio Ferreira <fabiodepaulo2008@gmail.com>
2024-07-18 22:00:59 -0300

    remoção do 'LocalDateTime'

--- src/main/java/com/fabio/todolist/webapptodo/service/TaskService.java
+++ src/main/java/com/fabio/todolist/webapptodo/service/TaskService.java
@@ -5,7 +5,6 @@ import com.fabio.todolist.webapptodo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
